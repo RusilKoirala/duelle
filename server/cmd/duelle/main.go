a7cf456d9ed58b80dabcfa67e94ec447752685e4
Rusil Koirala <rusilkoirala23@gmail.com>
2026-07-21 20:34:24 +0545

    implemented a basic server setupd :D

--- server/cmd/duelle/main.go
+++ server/cmd/duelle/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	// get the Port for the server to run in
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	// the basic server
+	mux := http.NewServeMux()
+
+	// i made frontend to load from backend / route like give static files
+	staticDir := http.Dir("./static")
+	staticHandler := http.FileServer(staticDir)
+	mux.Handle("/", staticHandler)
+
+	// health check point
+	mux.HandleFunc("/health", healthCheckHandler)
+
+	addr := fmt.Sprintf(":%s", port)
+
+	log.Printf("Server is running on http://localhost:%s", port)
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal("Server failed to start: %v", err)
+	}
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+
 }
--- server/internal/room/room.go
+++ server/internal/room/room.go
@@ -1,4 +1,14 @@
 package room
 
+import "sync"
+
 type Room struct {
+	ID         string
+	Players    [2]*Player
+	SecretWord string
+	State      string
+	mu         sync.Mutex
+}
+
+type Player struct {
 }
